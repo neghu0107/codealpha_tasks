2c455db1d622aa23ccb66cbe9f51c8ec4cac1b72
neha <neha.2401142@srec.ac.in>
2025-12-18 00:31:55 +0530

    Fix comment formatting in script.js

--- codealpha_translation_tool/script.js
+++ codealpha_translation_tool/script.js
@@ -132,7 +132,7 @@ swapBtn.addEventListener("click", () => {
   resultText.value = tempText;
 });
 
-/* ---------------------------
+/* ----------------------------
         ENTER SHORTCUT
 ----------------------------*/
 
